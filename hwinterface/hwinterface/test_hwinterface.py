29bc843f124c2533ef9b4a1e32ed679ec9b14b5f
JCallejox <javier.callejor@gmail.com>
2023-02-28 19:16:47 +0100

    hwinterface test v1

--- hwinterface/hwinterface/test_hwinterface.py
+++ hwinterface/hwinterface/test_hwinterface.py
@@ -1,169 +1,179 @@
 import rclpy
 from rclpy.node import Node
-
-from std_msgs.msg import String
+from rclpy.clock import Clock
+from datetime import datetime
+import serial
+import struct
+from math import copysign, pi
 from sensor_msgs.msg import JointState
 
 class HwClass(Node):
+
     def __init__(self):
+        # Call the constructor of the parent class
         super().__init__('hwinterface')
 
-        #self.read_encoder_freq = self.get_parameter('/diff_drive_controller/publish_rate').value
-        self.read_encoder_freq = 0.5
+        # Get the publish rate for encoder readings from the ROS 2 parameter server
+        self.read_encoder_freq = 6
 
-        #self.arduino = serial.Serial('/dev/arduino', 115200)
-        #data = self.arduino.readline()
-        #self.get_logger().info(data)
+        # Initialize variables for twist timeout and encoder parameters
+        self.lastTwistTime = 0
+        self.twistTimeout = 5
+        self.pasos_vuelta = 356.3 * 2
+        self.kdato = 36.28
 
-        # Create ROS2 publisher wheel state
+        # Open a serial connection to the Arduino device
+        self.arduino = serial.Serial('/dev/arduino', 115200)
+
+        # Read and discard the first line of data from the Arduino device
+        data = self.arduino.readline()
+
+        # Read the current encoder values
+        self.steps_enc_left, self.time_enc_left, self.steps_enc_right, self.time_enc_right = self._read_encoder()
+
+        # Create a ROS 2 publisher for wheel state information
         self._name_publisher = "wheel_state"
-
         self._wheel_pub = self.create_publisher(JointState, self._name_publisher, 10)
         self._timer = self.create_timer(self.read_encoder_freq, self._publisher_callback)
 
-        #create the subscriber
-        #self.create_subscription(JointState, "cmd_wheel", self.callback_vel, 1)
+        # Create a ROS 2 subscriber for wheel velocity commands
+        self._name_subscriber = "cmd_wheel"
+        self.create_subscription(JointState, self._name_subscriber, self._callback_vel, 1)
 
+    def _read_encoder(self):
+
+        # Send the 'N' character to the serial port to request encoder data
+        self.arduino.write(str('N').encode())
+
+        # Read the data from the serial port
+        # Read the first line, but discard the data
+        self.arduino.readline()
+
+        # Read the left encoder steps and time values
+        steps_enc_left = struct.unpack('i', self.arduino.read(4))[0]
+        time_enc_left = struct.unpack('i', self.arduino.read(4))[0]
+
+        # Read the right encoder steps and time values
+        steps_enc_right = struct.unpack('i', self.arduino.read(4))[0]
+        time_enc_right = struct.unpack('i', self.arduino.read(4))[0]
+
+        # Read the final line, but discard the data
+        self.arduino.readline()
+
+        return steps_enc_left, time_enc_left, steps_enc_right, time_enc_right
 
     def _publisher_callback(self):
 
+        # Check if it is still receiving data
+        self._check_alive()
+
+        # Read the encoder data
+        steps_enc_left, time_enc_left, steps_enc_right, time_enc_right = self._read_encoder()
+
+        # Calculate the elapsed time and distance travelled since the last measurement for the right wheel
+        dt_right = float((time_enc_right - self.time_enc_right) * (10 ** -6))
+        dsteps_right = float(steps_enc_right - self.steps_enc_right)
+
+        # Update the last measurement values for the right wheel
+        self.time_enc_right = time_enc_right
+        self.steps_enc_right = steps_enc_right
+
+        # Calculate the position and velocity for the right wheel
+        posD = (steps_enc_right / self.pasos_vuelta) * 2 * pi
+        wd = ((dsteps_right / self.pasos_vuelta) * 2 * pi) / dt_right
+
+        # Publish the right wheel position and velocity information
+        self._send_message('RIGHT', posD, wd)
+
+        # Calculate the elapsed time and distance travelled since the last measurement for the left wheel
+        dt_left = float((time_enc_left - self.time_enc_left) * (10 ** -6))
+        dsteps_left = float(steps_enc_left - self.steps_enc_left)
+
+        # Update the last measurement values for the left wheel
+        self.time_enc_left = time_enc_left
+        self.steps_enc_left = steps_enc_left
+
+        # Calculate the position and velocity for the left wheel
+        posI = (steps_enc_left / self.pasos_vuelta) * 2 * pi
+        wi = ((dsteps_left / self.pasos_vuelta) * 2 * pi) / dt_left
+
+        # Publish the left wheel position and velocity information
+        self._send_message('LEFT', posI, wd)
+
+        # Print velocity information
+        self.get_logger().info(f" {wi}, {wd}")
+
+    def _send_message(self, name, pos, w):
+        # Create a new JointState message object
         msg = JointState()
-        #msg.data = 'Hello World: %d' % self.i
 
+        # Set the values of the JointState message object
+        msg.name = [name]  # Set the name of the joint (LEFT or RIGHT)
+        msg.position = [pos]  # Set the position value in radians
+        msg.velocity = [w]  # Set the velocity value in radians per second
+        msg.header.stamp = self.get_clock().now().to_msg()  # Set the timestamp
+
+        # Publish the JointState message to the 'wheel_state' topic
         self._wheel_pub.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg)
 
-    """
-    def dum_first_read(self):
-        self.get_logger().info("LECTURA DUMMY")
-        #send the N letter to the serial port
-        self.arduino.write(str('N').encode())
-        
-        #comienzo = self.arduino.readline()
-        steps_enc_left = struct.unpack('i', self.arduino.read(4))[0]
-        time_enc_left = struct.unpack('i', self.arduino.read(4))[0]
-        steps_enc_right = struct.unpack('i', self.arduino.read(4))[0]
-        time_enc_right = struct.unpack('i', self.arduino.read(4))[0]
-        #final = self.arduino.readline()
-        self.time_enc_right_last = time_enc_right
-        self.steps_enc_right_last = steps_enc_right
-        self.time_enc_left_last = time_enc_left
-        self.steps_enc_left_last = steps_enc_left
-        self.get_logger().info(str(steps_enc_right))
+    def _check_alive(self):
+        # Get the current time in seconds
+        current_time = datetime.now().second
 
-        self.steps_enc_left_last = struct.unpack('i', self.arduino.read(4))[0]
-        self.time_enc_left_last = struct.unpack('i', self.arduino.read(4))[0]
-        self.steps_enc_right_last = struct.unpack('i', self.arduino.read(4))[0]
-        self.time_enc_right_last = struct.unpack('i', self.arduino.read(4))[0]
-
-    def check(self):
-        current_time = self.get_clock_ros().now().nanoseconds
-        if (current_time - self.lastTwistTime) > self.twistTimeout * 1e9:
-            #send ? to the serial port
+        # Check if the time since the last twist message exceeds the timeout value
+        if (current_time - self.lastTwistTime) > self.twistTimeout:
+            # Send a "?" character to the Arduino device to request new data
             self.arduino.write(str('?').encode())
+
+            # Log a message indicating that the robot has stopped due to not receiving data
             self.get_logger().info("Parada por no recibir datos")
 
-    def callback_vel(self, msg):
+    def _callback_vel(self, msg):
+        # Define a lambda function to get the sign of a number
         sign = lambda x: int(copysign(1, x))
-        # Save variables
-        comandD = msg.velocity[self.right]  # rad/s
-        comandI = msg.velocity[self.left]  # rad/s
 
-        # Convert from rad/s to data 0-127
+        # Extract the desired angular velocities from the JointState message
+        comandD = msg.velocity[0]  # rad/s
+        comandI = msg.velocity[1]  # rad/s
+
+        # Convert the angular velocities from rad/s to an integer data value in the range 0-127
         datod = int(round(comandD * self.kdato))
         datoi = int(round(comandI * self.kdato))
 
-      
-        # Saturation of data
-        if abs(datod) > 127:
-            datod = 127 * sign(datod)
-        if abs(datoi) > 127:
-            datoi = 127 * sign(datoi)
-
-        # Minimum velocity assigned
-        if (abs(datod) < 10 and datod != 0):
-            datod = 10 * sign(datod)
-        if (abs(datoi) < 10 and datoi != 0):
-            datoi = 10 * sign(datoi)
-
-        # Negative velocity convert
-        if datod < 0:
-            datod = 256 + datod
-        if datoi < 0:
-            datoi = 256 + datoi
-       
+        # Limit the data value to the range 10-127 and adjust the sign if necessary
         datod = min(127, max(10 * sign(datod), datod)) if datod != 0 else 0
         datoi = min(127, max(10 * sign(datoi), datoi)) if datoi != 0 else 0
+
+        # Add an offset of 256 to the data value if it is negative
         datod = 256 + datod if datod < 0 else datod
         datoi = 256 + datoi if datoi < 0 else datoi
 
-        # TODO: Analizar si se debe enviar algun mecanismo de eco
-        self.arduino.write(('V' + format(int(datod), '03d') +
-                            format(datoi, '03d')).encode())
+        # Log the data values (for debugging purposes)
+        self.get_logger().info(f"datod: {datod} y datoi: {datoi}")
 
-        # Se toma para ver si hay un Timeout.
-        #self.lastTwistTime = self.get_clock().now().seconds
+        # Send the data values to the Arduino device in the format 'V###' using a serial connection
+        self.arduino.write(('V' + format(int(datod), '03d') + format(int(datoi), '03d')).encode())
 
-
-    def read_encoders(self):
-
-        self.arduino.write(str('N').encode())
-        comienzo = self.arduino.readline()
-        steps_enc_left = struct.unpack('i', self.arduino.read(4))[0]
-        time_enc_left = struct.unpack('i', self.arduino.read(4))[0]
-        steps_enc_right = struct.unpack('i', self.arduino.read(4))[0]
-        time_enc_right = struct.unpack('i', self.arduino.read(4))[0]
-        final = self.arduino.readline()
-        data = JointState()
-
-        dt_right = float((time_enc_right - self.time_enc_right_last) * (10 ** -6))
-        dsteps_right = float(steps_enc_right - self.steps_enc_right_last)
-        self.time_enc_right_last = time_enc_right
-        self.steps_enc_right_last = steps_enc_right
-
-        posD = (steps_enc_right / self.pasos_vuelta) * 2 * self.pi
-        wd = ((dsteps_right / self.pasos_vuelta) * 2 * self.pi) / dt_right
-
-        data.name = ["RIGHT"]
-        data.position = [posD]
-        data.velocity = [wd]
-        data.effort = [0]
-        data.header.stamp = self.get_clock_ros().now().to_msg()
-        data.header.frame_id = "base_link"
-        self.data_pub.publish(data)
-
-        dt_left = float((time_enc_left - self.time_enc_left_last) * (10 ** -6))
-        dsteps_left = float(steps_enc_left - self.steps_enc_left_last)
-
-        self.time_enc_left_last = time_enc_left
-        self.steps_enc_left_last = steps_enc_left
-
-        posI = (steps_enc_left / self.pasos_vuelta) * 2 * self.pi
-        wi = ((dsteps_left / self.pasos_vuelta) * 2 * self.pi) / dt_left
-
-        data = JointState()
-        data.name = ["LEFT"]
-        data.position = [posI]
-        data.velocity = [wi]
-        data.effort = [0]
-        data.header.stamp = self.node.get_clock().now().to_msg()
-        data.header.frame_id = "base_link"
-
-        #publish topic
-        self.data_pub.publish(data)
-        self.get_logger().info("WI: %.2f   WD: %.2f" % (wi, wd))
-    """
+        # Update the last twist time to the current time
+        self.lastTwistTime = datetime.now().second
 
 
 def main(args=None):
+    # Initialize the ROS2 system with the given arguments
     rclpy.init(args=args)
 
+    # Create an instance of the HwClass node implementation
     minimal_subscriber = HwClass()
 
+    # Start the ROS2 event loop and wait for it to stop
     rclpy.spin(minimal_subscriber)
 
+    # Clean up the node resources when the event loop is stopped
     minimal_subscriber.destroy_node()
+
+    # Shut down the ROS2 system
     rclpy.shutdown()
+
 
 if __name__ == '__main__':
     main()
